7da2b805f8c623535853b24a034d9f82c0bfbde7
Master Joy <386442876@qq.com>
2024-10-30 09:13:47 +0800

    优化：完善K8S模板增删改查功能

--- logic/template/list_logic.go
+++ logic/template/list_logic.go
@@ -1,18 +1,30 @@
 package template
 
 import (
+	"fmt"
+
 	"k8s-deploy/query"
 	"k8s-deploy/svc"
 	"k8s-deploy/types"
 
 	"github.com/jinzhu/copier"
+	"gorm.io/gen"
 )
 
 // List 模板列表
 func List(ctx *svc.ServiceContext, req *types.TemplateListRequest) (resp types.TemplateListResponse, err error) {
 	k8sTemplateModel := query.K8sTemplateModel
 
+	var where []gen.Condition
+
+	if len(req.Name) > 0 {
+		where = append(where, k8sTemplateModel.WithContext(ctx).
+			Where(k8sTemplateModel.Name.Like(fmt.Sprintf("%%%s%%", req.Name))).
+			Or(k8sTemplateModel.Desc.Like(fmt.Sprintf("%%%s%%", req.Name))))
+	}
+
 	data, count, _ := k8sTemplateModel.WithContext(ctx).
+		Where(where...).
 		Order(k8sTemplateModel.ID.Desc()).
 		FindByPage((req.Page-1)*req.PageSize, req.PageSize)
 
