3868892aa2bd546ca76092a0a93e672085d00030
Master Joy <386442876@qq.com>
2024-11-06 15:04:29 +0800

    优化：前端配置

--- ui/static/404-b894b336.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,d as s,f as n,k as o}from"./index-cc90a63c.js";const t={},a={class:"my-404-container"};function r(d,i){const _=o;return c(),s("div",a,[n(_,{"icon-class":"404",class:"my-404-img"})])}const p=e(t,[["render",r],["__scopeId","data-v-66bf2050"]]);export{p as default};
